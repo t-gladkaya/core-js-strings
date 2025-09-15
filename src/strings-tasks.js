9a1e042f934f82ef1f5171c6f304aaf9c2838b3f
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2025-09-15 22:20:21 +0300

    feat: implement getFirstChar and removeLeadingAndTrailingWhitespaces function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -71,8 +71,8 @@ function concatenateStrings(value1, value2) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value.charAt(0);
 }
 
 /**
@@ -86,8 +86,8 @@ function getFirstChar(/* value */) {
  *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
  *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
 }
 
 /**

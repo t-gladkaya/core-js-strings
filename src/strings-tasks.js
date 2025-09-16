6459e216a00c97825872ccfbb4e9ec522032a093
Tatsiana Hladkaya <tatyana.hladkaya@gmail.com>
2025-09-16 13:10:11 +0300

    feat: implement encodeToRot13 function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -480,8 +480,20 @@ function extractEmails(str) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+function encodeToRot13(str) {
+  return str
+    .split('')
+    .map((char) => {
+      const code = char.charCodeAt(0);
+      if (code >= 65 && code <= 90) {
+        return String.fromCharCode(((code - 65 + 13) % 26) + 65);
+      }
+      if (code >= 97 && code <= 122) {
+        return String.fromCharCode(((code - 97 + 13) % 26) + 97);
+      }
+      return char;
+    })
+    .join('');
 }
 
 /**
